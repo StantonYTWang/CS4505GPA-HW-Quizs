1bce25ee77731d293f00811e16d7b3b27aed6b6c
StantonYTWang <auop.321@gmail.com>
2016-04-25 18:30:54 +0800

    Added files via upload

---
+++ Quiz/Quiz-7/Quiz Framework - Shader/VC10/Quiz_Framework/main.cpp
@@ -0,0 +1,255 @@
+#include <glew.h>
+#include <freeglut.h>
+#define GLM_SWIZZLE
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <cstdio>
+#include <cstdlib>
+#include <texture_loader.h>
+#include "data.h"
+
+#define MENU_TIMER_START 1
+#define MENU_TIMER_STOP 2
+#define MENU_EXIT 3
+
+GLubyte timer_cnt = 0;
+bool timer_enabled = true;
+unsigned int timer_speed = 16;
+
+using namespace glm;
+
+mat4 mvp;
+GLint um4mvp;
+
+void checkError(const char *functionName)
+{
+    GLenum error;
+    while (( error = glGetError() ) != GL_NO_ERROR) {
+        fprintf (stderr, "GL error 0x%X detected in %s\n", error, functionName);
+    }
+}
+
+// Print OpenGL context related information.
+void dumpInfo(void)
+{
+    printf("Vendor: %s\n", glGetString (GL_VENDOR));
+    printf("Renderer: %s\n", glGetString (GL_RENDERER));
+    printf("Version: %s\n", glGetString (GL_VERSION));
+    printf("GLSL: %s\n", glGetString (GL_SHADING_LANGUAGE_VERSION));
+}
+
+char** loadShaderSource(const char* file)
+{
+	FILE* fp = fopen(file, "rb");
+	fseek(fp, 0, SEEK_END);
+	long sz = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+	char *src = new char[sz + 1];
+	fread(src, sizeof(char), sz, fp);
+	src[sz] = '\0';
+	char **srcp = new char*[1];
+	srcp[0] = src;
+	return srcp;
+}
+
+void freeShaderSource(char** srcp)
+{
+	delete srcp[0];
+	delete srcp;
+}
+
+void shaderLog(GLuint shader)
+{
+	GLint isCompiled = 0;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &isCompiled);
+	if(isCompiled == GL_FALSE)
+	{
+		GLint maxLength = 0;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &maxLength);
+
+		// The maxLength includes the NULL character
+		GLchar* errorLog = new GLchar[maxLength];
+		glGetShaderInfoLog(shader, maxLength, &maxLength, &errorLog[0]);
+
+		printf("%s\n", errorLog);
+		delete errorLog;
+	}
+}
+
+void My_Init()
+{
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+
+    GLuint program = glCreateProgram();
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	char** vertexShaderSource = loadShaderSource("vertex.vs.glsl");
+	char** fragmentShaderSource = loadShaderSource("fragment.fs.glsl");
+    glShaderSource(vertexShader, 1, vertexShaderSource, NULL);
+    glShaderSource(fragmentShader, 1, fragmentShaderSource, NULL);
+	freeShaderSource(vertexShaderSource);
+	freeShaderSource(fragmentShaderSource);
+    glCompileShader(vertexShader);
+    glCompileShader(fragmentShader);
+	shaderLog(vertexShader);
+    shaderLog(fragmentShader);
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+    glLinkProgram(program);
+	um4mvp = glGetUniformLocation(program, "um4mvp");
+    glUseProgram(program);
+    
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    
+	//int i;
+	//for(i=0; i<24876; i+=18){
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), 0);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float),(void*)(sizeof(float) * 3));
+	//}
+	
+
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    
+    GLuint buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
+}
+
+// GLUT callback. Called to draw the scene.
+void My_Display()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glDrawArrays(GL_TRIANGLES, 0, 24876);
+	glutSwapBuffers();
+}
+
+void My_Reshape(int width, int height)
+{
+	glViewport(0, 0, width, height);
+
+	float viewportAspect = (float)width / (float)height;
+	mvp = ortho(-20 * viewportAspect, 20 *viewportAspect, -20.0f, 20.0f, 0.0f, 50.0f) *lookAt(vec3(0.0f, 20.0f, 20.0f), vec3(0.0f, 0.0f,0.0f), vec3(0.0f, 1.0f, 0.0f)) * translate(mat4(),vec3(0, 0, 10));
+}
+
+void My_Timer(int val)
+{
+	timer_cnt++;
+	glutPostRedisplay();
+	if(timer_enabled)
+	{
+		glutTimerFunc(timer_speed, My_Timer, val);
+	}
+}
+
+void My_Mouse(int button, int state, int x, int y)
+{
+	if(state == GLUT_DOWN)
+	{
+		printf("Mouse %d is pressed at (%d, %d)\n", button, x, y);
+	}
+	else if(state == GLUT_UP)
+	{
+		printf("Mouse %d is released at (%d, %d)\n", button, x, y);
+	}
+}
+
+void My_Keyboard(unsigned char key, int x, int y)
+{
+	printf("Key %c is pressed at (%d, %d)\n", key, x, y);
+}
+
+void My_SpecialKeys(int key, int x, int y)
+{
+	switch(key)
+	{
+	case GLUT_KEY_F1:
+		printf("F1 is pressed at (%d, %d)\n", x, y);
+		break;
+	case GLUT_KEY_PAGE_UP:
+		printf("Page up is pressed at (%d, %d)\n", x, y);
+		break;
+	case GLUT_KEY_LEFT:
+		printf("Left arrow is pressed at (%d, %d)\n", x, y);
+		break;
+	default:
+		printf("Other special key is pressed at (%d, %d)\n", x, y);
+		break;
+	}
+}
+
+void My_Menu(int id)
+{
+	switch(id)
+	{
+	case MENU_TIMER_START:
+		if(!timer_enabled)
+		{
+			timer_enabled = true;
+			glutTimerFunc(timer_speed, My_Timer, 0);
+		}
+		break;
+	case MENU_TIMER_STOP:
+		timer_enabled = false;
+		break;
+	case MENU_EXIT:
+		exit(0);
+		break;
+	default:
+		break;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	// Initialize GLUT and GLEW, then create a window.
+	////////////////////
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(600, 600);
+	glutCreateWindow("Quiz Framework"); // You cannot use OpenGL functions before this line; The OpenGL context must be created first by glutCreateWindow()!
+	glewInit();
+	dumpInfo();
+	My_Init();
+	////////////////////
+
+	// Create a menu and bind it to mouse right button.
+	////////////////////////////
+	int menu_main = glutCreateMenu(My_Menu);
+	int menu_timer = glutCreateMenu(My_Menu);
+
+	glutSetMenu(menu_main);
+	glutAddSubMenu("Timer", menu_timer);
+	glutAddMenuEntry("Exit", MENU_EXIT);
+
+	glutSetMenu(menu_timer);
+	glutAddMenuEntry("Start", MENU_TIMER_START);
+	glutAddMenuEntry("Stop", MENU_TIMER_STOP);
+
+	glutSetMenu(menu_main);
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+	////////////////////////////
+
+	// Register GLUT callback functions.
+	///////////////////////////////
+	glutDisplayFunc(My_Display);
+	glutReshapeFunc(My_Reshape);
+	glutMouseFunc(My_Mouse);
+	glutKeyboardFunc(My_Keyboard);
+	glutSpecialFunc(My_SpecialKeys);
+	glutTimerFunc(timer_speed, My_Timer, 0); 
+	///////////////////////////////
+
+	// Enter main event loop.
+	//////////////
+	glutMainLoop();
+	//////////////
+	return 0;
+}
